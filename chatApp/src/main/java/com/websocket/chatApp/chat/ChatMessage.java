1b48a5ba8568be223576c2ede444f7544653744f
Shalin Dileesha <135975902+dileesha-shalin@users.noreply.github.com>
2024-09-07 11:58:25 +0530

    Add files via upload

---
+++ chatApp/src/main/java/com/websocket/chatApp/chat/ChatMessage.java
@@ -0,0 +1,19 @@
+package com.websocket.chatApp.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+
+    private MessageType type;
+
+
+}
---
+++ chatApp/src/main/java/com/websocket/chatApp/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.websocket.chatApp.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
